80a224848396736895b331510ea5206f083b1d2f
Sam Atkinson <manamissam@gmail.com>
2017-04-20 17:20:06 -0400

    Implemented branching

--- src/lightning.cpp
+++ src/lightning.cpp
@@ -13,29 +13,40 @@ float rand_float() {
 
 
 void Mesh::addLightning(glm::vec3 start_pos) {
-  
-  // Lightning properties 
-  float mean_seg_length = 0.08;
-  float start_radius = 0.04;
-  float max_angle_degrees = 50.0;
+ 
+  // Center branch goes from starting position to ground plane 
   glm::vec3 dir(0,-1,0);
+  float dist = glm::distance(start_pos, glm::vec3(0,0,0));
+  float branch_probability = 0.2;
+  float mean_branch_length = 2.0;
+  addBranch(start_pos, dir, dist, branch_probability, mean_branch_length);
+
+}
+
+
+void Mesh::addBranch(glm::vec3 start_pos, glm::vec3 dir, float dist,
+                     float branch_probability, float mean_branch_length) {
+
+  // Branch properties
+  float start_radius = 0.04;
+  float mean_seg_length = 0.08;
+  float max_seg_angle_degrees = 40.0;
+  float max_branch_angle_degrees = 20.0;
   glm::vec3 rotation_normal(0,0,1);
 
-  // Variables to track lightning construction
-  float height, angle, seglength, radius;
-  glm::vec3 last, next;
+  // Variables to track branch construction
+  float angle, seglength, radius, branch_angle, branch_dist;
+  glm::vec3 last, next, branch;
 
-  // Initialize 
-  height = start_pos.y;
-  radius = start_radius;
+  // Initialize
+  next = start_pos;
   last = start_pos;
-
-  //lightning_segments.push_back(LightningSegment(radius, last, last + dir));
+  radius = start_radius;
 
   // Create segments
-  while (height > 0) {
-    // Random segment angle 
-    angle = (0.5 - rand_float()) * 2.0 * max_angle_degrees;
+  while (glm::distance(next, start_pos) < dist) {
+    // Random segment angle
+    angle = (0.5 - rand_float()) * 2.0 * max_seg_angle_degrees;         
     angle = angle * (M_PI / 180.0);
     // Random segment length
     seglength = rand_float() * 2.0 * mean_seg_length;
@@ -45,10 +56,18 @@ void Mesh::addLightning(glm::vec3 start_pos) {
     next = next + last;
     // Create segment and add to mesh
     lightning_segments.push_back(LightningSegment(radius, last, next));
+    // Recursively add branches
+    if (rand_float() < branch_probability && branch_probability > 0.01) {
+      branch_angle = (0.5 - rand_float())  * max_branch_angle_degrees;
+      branch_angle = branch_angle * (M_PI / 180.0);
+      branch_dist = rand_float() * 2.0 * mean_branch_length;
+      branch = glm::rotate(next-last, branch_angle, rotation_normal);
+      branch = glm::normalize(branch);
+      addBranch(next, branch, branch_dist, branch_probability*0.2, 
+                mean_branch_length*0.5);
+    }
     // Update for next iteration
-    height -= (last.y - next.y); 
     last = next;
-    radius -= 0.0004;
   }
 
 }
